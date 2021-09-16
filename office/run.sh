de6505ab16314720556dd1c302ac8f6129e536a1
gpx <1457495424@qq.com>
2021-09-16 08:54:26 +0800

    modify run.sh

--- office/run.sh
+++ office/run.sh
@@ -15,6 +15,12 @@ python -u train.py --dataset office-31 --model AMTL --version v2 --gpu_id 3 > ou
 
 python -u train.py --dataset office-31 --model AMTL --version v3 --gpu_id 3 > out/office31_amtl_v3.out
 
+python -u train.py --dataset office-31 --model AMTL_new --version v1 --gpu_id 3 > out/office31_AMTL_new_v1.out
+
+python -u train.py --dataset office-31 --model AMTL_new --version v2 --gpu_id 3 > out/office31_AMTL_new_v2.out
+
+python -u train.py --dataset office-31 --model AMTL_new --version v3 --gpu_id 3 > out/office31_AMTL_new_v3.out
+
 # office-home
 python -u train.py --dataset office-home --model DMTL --gpu_id 3 --task_index 0 > out/officehome_single_task1.out
 
@@ -33,3 +39,9 @@ python -u train.py --dataset office-home --model AMTL --version v1 --gpu_id 3 > 
 python -u train.py --dataset office-home --model AMTL --version v2 --gpu_id 3 > out/officehome_amtl_v2.out
 
 python -u train.py --dataset office-home --model AMTL --version v3 --gpu_id 3 > out/officehome_amtl_v3.out
+
+python -u train.py --dataset office-home --model AMTL_new --version v1 --gpu_id 3 > out/officehome_AMTL_new_v1.out
+
+python -u train.py --dataset office-home --model AMTL_new --version v2 --gpu_id 3 > out/officehome_AMTL_new_v2.out
+
+python -u train.py --dataset office-home --model AMTL_new --version v3 --gpu_id 3 > out/officehome_AMTL_new_v3.out
