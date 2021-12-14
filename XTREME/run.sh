3f2a0247de3ab40b8561904ea509d2fdd8dc6807
gpx <1457495424@qq.com>
2021-12-14 09:53:11 +0800

    add NER task in run.sh

--- XTREME/run.sh
+++ XTREME/run.sh
@@ -21,3 +21,14 @@ python train.py --gpu_id 1 --model STL --dataset pawsx --lang de  > out/stl_paws
 python train.py --gpu_id 6 --model STL --dataset pawsx --lang es  > out/stl_pawsx_es.out
 
 python train.py --gpu_id 0 --model DMTL --dataset pawsx > out/dmtl_pawsx_all.out
+
+# for NER task
+python train.py --gpu_id 0 --model STL --dataset panx --lang en  > out/stl_panx_en.out
+
+python train.py --gpu_id 7 --model STL --dataset panx --lang zh  > out/stl_panx_zh.out
+
+python train.py --gpu_id 2 --model STL --dataset panx --lang de  > out/stl_panx_de.out
+
+python train.py --gpu_id 5 --model STL --dataset panx --lang es  > out/stl_panx_es.out
+
+python train.py --gpu_id 1 --model DMTL --dataset panx > out/dmtl_panx_all.out
