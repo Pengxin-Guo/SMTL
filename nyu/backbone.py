0b944c2ac2ea62a13a49af10f5e8cbce1cdbee63
gpx <1457495424@qq.com>
2021-12-16 21:13:30 +0800

    add some weight strategy

--- nyu/backbone.py
+++ nyu/backbone.py
@@ -41,9 +41,6 @@ class DeepLabv3(nn.Module):
     
     def predict(self, x):
         return self.forward(x)
-    
-    def get_share_params(self):
-        return self.backbone.parameters()
         
 
 class Cross_Stitch(nn.Module):
@@ -242,27 +239,6 @@ class MTANDeepLabv3(nn.Module):
         downsample = nn.Sequential(conv1x1(in_channel, 4 * out_channel, stride=1),
                                    nn.BatchNorm2d(4 * out_channel))
         return Bottleneck(in_channel, out_channel, downsample=downsample)
-    
-    def get_share_params(self):
-        p = []
-        p += self.shared_conv.parameters()
-        p += self.shared_layer1_b.parameters()
-        p += self.shared_layer2_b.parameters()
-        p += self.shared_layer3_b.parameters()
-        p += self.shared_layer4_b.parameters()
-        p += self.shared_layer1_t.parameters()
-        p += self.shared_layer2_t.parameters()
-        p += self.shared_layer3_t.parameters()
-        p += self.shared_layer4_t.parameters()
-        p += self.encoder_att_1.parameters()
-        p += self.encoder_att_2.parameters()
-        p += self.encoder_att_3.parameters()
-        p += self.encoder_att_4.parameters()
-        p += self.encoder_block_att_1.parameters()
-        p += self.encoder_block_att_2.parameters()
-        p += self.encoder_block_att_3.parameters()
-        p += self.down_sampling.parameters()
-        return p
     
     def predict(self, x):
         return self.forward(x)
@@ -509,9 +485,6 @@ class SMTLmodel(nn.Module):
             if t == 'normal':
                 out[i] = out[i] / torch.norm(out[i], p=2, dim=1, keepdim=True)
         return out
-    
-    def get_share_params(self):
-        return self.backbone.parameters()
         
     def get_adaptative_parameter(self):
         return self.alpha
@@ -639,10 +612,278 @@ class SMTLmodel_new(nn.Module):
                 out[i] = out[i] / torch.norm(out[i], p=2, dim=1, keepdim=True)
         
         return out
-    
-    def get_share_params(self):
-        return self.backbone.parameters()
         
     def get_adaptative_parameter(self):
         return self.alpha
+
+
+# do selection at hidden layer
+class SMTLmodel_weight(nn.Module):
+    def __init__(self, dataset='NYUv2', version='v1', weighting='EW'):
+        super(SMTLmodel_weight, self).__init__()        
+        self.version = version
+               
+        if dataset == 'NYUv2':
+            self.class_nb = 13
+            self.tasks = ['segmentation', 'depth', 'normal']
+            self.num_out_channels = {'segmentation': 13, 'depth': 1, 'normal': 3}
+        elif dataset == 'CityScape':
+            self.class_nb = 7
+            self.tasks = ['segmentation', 'depth']
+            self.num_out_channels = {'segmentation': 7, 'depth': 1}
+        else:
+            raise('No support {} dataset'.format(dataset))
         
+        # for different weighting
+        rep_detach = True if weighting in ['GradDrop', 'IMTL', 'MGDA_approx', 'GradNorm'] else False
+        if weighting == 'IMTL':
+            loss_weight_init = 0.0
+        elif weighting == 'UW':
+            loss_weight_init = -0.5
+        elif weighting == 'WGLS':
+            loss_weight_init = 1.0
+        elif weighting == 'GradNorm':
+            loss_weight_init = 1.0
+        else:
+            loss_weight_init = None
+        m_weighting = 'GMM' if weighting == 'GMM' else None
+        
+        if rep_detach:
+            self.rep = [0] * len(self.tasks)
+            self.rep_i = [0] * len(self.tasks)
+        if isinstance(loss_weight_init, float):
+           self.loss_scale = nn.Parameter(torch.FloatTensor([loss_weight_init] * len(self.tasks)))
+        if weighting == 'GMM':
+            self.mix_k = 100
+            self.comp_mu = nn.Parameter(torch.ones(self.mix_k, len(self.tasks)))
+            self.comp_sigma = nn.Parameter(torch.zeros(self.mix_k, len(self.tasks)))
+        
+        # shared encoder
+        self.backbone_s = ResnetDilated(resnet.__dict__['resnet50'](pretrained=True))
+        ch = [256, 512, 1024, 2048]
+        
+        # task-specific encoder
+        self.backbone_t = nn.ModuleList([ResnetDilated(resnet.__dict__['resnet50'](pretrained=True)) for _ in self.tasks])
+        
+        # adaptative parameters
+        if self.version == 'v1' or self.version =='v2':
+            # SMTL-v1 and v2
+            self.alpha = nn.Parameter(torch.FloatTensor(len(self.tasks), 2))
+            self.alpha.data.fill_(0.5)   # init 0.5(shared) 0.5(specific)
+            # self.alpha.data[:,0].fill_(0)  # shared
+            # self.alpha.data[:,1].fill_(1)  # specific
+        elif self.version == 'v3':
+            # SMTL-v3, gumbel softmax
+            self.alpha = nn.Parameter(torch.FloatTensor(len(self.tasks)))
+            self.alpha.data.fill_(0)
+        else:
+            print("No correct version parameter!")
+            exit()
+        
+        # task-specific decoder
+        self.decoders = nn.ModuleList([DeepLabHead(2048, self.num_out_channels[t]) for t in self.tasks])
+        
+    def forward(self, x):
+        img_size  = x.size()[-2:]
+        # shared encoder output
+        x_s = self.backbone_s(x)
+        # task-specific encoder output
+        x_t = [0 for _ in self.tasks]
+        for i in range(len(self.tasks)):
+            x_t[i] = self.backbone_t[i](x)
+        # combine shared encoder output and task-specific encoder output, obtain final hidden feature
+        x_h = [0 for _ in self.tasks]
+        for i in range(len(self.tasks)):
+            if self.version == 'v1':
+                temp_alpha = F.softmax(self.alpha[i], 0)     # SMTL-v1, alpha_1 + alpha_2 = 1
+            elif self.version == 'v2':
+                temp_alpha = torch.exp(self.alpha[i]) / (1 + torch.exp(self.alpha[i])) # SMTL-v2, 0 <= alpha <=1
+            elif self.version == 'v3':
+                # below for SMTL-v3, gumbel softmax
+                temp = torch.sigmoid(self.alpha[i])
+                temp_alpha = torch.stack([1-temp, temp])
+                temp_alpha = F.gumbel_softmax(torch.log(temp_alpha), tau=0.1, hard=True)
+            else:
+                print("No correct version parameter!")
+                exit()
+
+            x_h[i] = temp_alpha[0] * x_s + temp_alpha[1] * x_t[i]
+        out = [0 for _ in self.tasks]
+        for i, t in enumerate(self.tasks):
+            out[i] = F.interpolate(self.decoders[i](x_h[i]), img_size, mode='bilinear', align_corners=True)
+            if t == 'segmentation':
+                out[i] = F.log_softmax(out[i], dim=1)
+            if t == 'normal':
+                out[i] = out[i] / torch.norm(out[i], p=2, dim=1, keepdim=True)
+        return out
+        
+    def predict(self, x):
+        img_size  = x.size()[-2:]
+        # shared encoder output
+        x_s = self.backbone_s(x)
+        # task-specific encoder output
+        x_t = [0 for _ in self.tasks]
+        for i in range(len(self.tasks)):
+            x_t[i] = self.backbone_t[i](x)
+        # combine shared encoder output and task-specific encoder output, obtain final hidden feature
+        x_h = [0 for _ in self.tasks]
+        for i in range(len(self.tasks)):
+            if self.version == 'v1':
+                temp_alpha = F.softmax(self.alpha[i], 0)     # SMTL-v1, alpha_1 + alpha_2 = 1
+            elif self.version == 'v2':
+                temp_alpha = torch.exp(self.alpha[i]) / (1 + torch.exp(self.alpha[i])) # SMTL-v2, 0 <= alpha <=1
+            elif self.version == 'v3':
+                # below for SMTL-v3, gumbel softmax
+                temp = torch.sigmoid(self.alpha[i])
+                if temp >= 0.5:
+                    temp_alpha = [0, 1]
+                else:
+                    temp_alpha = [1, 0]
+            else:
+                print("No correct version parameter!")
+                exit()
+
+            x_h[i] = temp_alpha[0] * x_s + temp_alpha[1] * x_t[i]
+        out = [0 for _ in self.tasks]
+        for i, t in enumerate(self.tasks):
+            out[i] = F.interpolate(self.decoders[i](x_h[i]), img_size, mode='bilinear', align_corners=True)
+            if t == 'segmentation':
+                out[i] = F.log_softmax(out[i], dim=1)
+            if t == 'normal':
+                out[i] = out[i] / torch.norm(out[i], p=2, dim=1, keepdim=True)
+        return out
+    
+    def GradDrop_backward(self, losses, leak=0.0):
+        epsilon = 1e-7
+        if isinstance(self.rep, list):
+            batch_size = self.rep[0].size()[0]
+            rep_size = self.rep[0].size()
+            inputs = torch.stack(self.rep).reshape(len(self.tasks), batch_size, -1)
+        else:
+            batch_size = self.rep.size()[0]
+            rep_size = self.rep.size()
+            inputs = self.rep.reshape(batch_size, -1).unsqueeze(0).repeat(len(self.tasks), 1, 1)
+        per_loss_grads = torch.zeros(len(self.tasks), *rep_size).cuda()
+        for tn, loss in enumerate(losses):
+            loss.backward(retain_graph=True)
+            per_loss_grads[tn] = self.rep_i[tn].grad.data.clone()
+        grads = per_loss_grads.reshape(len(self.tasks), batch_size, -1)
+        inputs_abs = torch.abs((torch.abs(inputs)<=epsilon).float()+inputs)
+        grads = grads*(inputs/inputs_abs)
+        grads = grads.sum(1)
+
+        grad_sign_positive = (grads>0.0).float()
+        grad_sign_negative = (grads<0.0).float()
+
+        prob_pos = grads.sum(0)/(2*grads.abs().sum(0)+epsilon)+0.5
+        prob_pos = (prob_pos>=torch.rand(prob_pos.shape).cuda()).float()-0.5
+
+        grad_masks = (((grad_sign_positive-grad_sign_negative)*prob_pos)>=0).float()
+        grad_masks = grad_masks.unsqueeze(1).repeat(1, batch_size, 1).reshape(*per_loss_grads.size())
+        
+        transformed_per_loss_grads = per_loss_grads*(leak+(1-leak)*grad_masks)
+        
+        if isinstance(self.rep, list):
+            for tn in range(len(self.tasks)):
+                self.rep[tn].backward(transformed_per_loss_grads[tn], retain_graph=True)
+        else:
+            self.rep.backward(transformed_per_loss_grads.sum(0))
+    
+        # transformed_grad_norm = (transformed_grad**2).sum().sqrt()
+        # original_grad_norm = (grad_output**2).sum().sqrt()
+
+        # grad_input = transformed_grad*original_grad_norm/(transformed_grad_norm+epsilon)
+#         self.rep.backward(transformed_grad)
+        
+        self.rep_i = [0]*len(self.tasks)
+    
+    def IMTL_backward(self, losses):
+        if isinstance(self.rep, list):
+            batch_size = self.rep[0].size()[0]
+            rep_size = self.rep[0].size()
+        else:
+            batch_size = self.rep.size()[0]
+            rep_size = self.rep.size()
+        per_loss_grads = torch.zeros(len(self.tasks), *rep_size).cuda()
+        for tn, loss in enumerate(losses):
+            loss = torch.exp(self.loss_scale[tn])*loss - self.loss_scale[tn]
+            loss.backward(retain_graph=True)
+            per_loss_grads[tn] = self.rep_i[tn].grad.data.clone()
+        grads = per_loss_grads.reshape(len(self.tasks), batch_size, -1).sum(1)
+        grads_unit = grads/(torch.norm(grads, p=2, dim=-1, keepdim=True)+1e-8)
+
+        D = grads[0:1].repeat(len(self.tasks)-1, 1) - grads[1:]
+        U = grads_unit[0:1].repeat(len(self.tasks)-1, 1) - grads_unit[1:]
+
+        alpha = torch.matmul(torch.matmul(grads[0], U.t()), torch.linalg.inv(torch.matmul(D, U.t())))
+        alpha = torch.cat((1-alpha.sum().unsqueeze(0), alpha), dim=0)
+        
+        if isinstance(self.rep, list):
+            for tn in range(len(self.tasks)):
+                self.rep[tn].backward(alpha[tn]*per_loss_grads[tn], retain_graph=True)
+        else:
+            transformed_grad = sum(alpha[tn]*per_loss_grads[tn] for tn in range(len(self.tasks)))
+            self.rep.backward(transformed_grad)
+        self.rep_i = [0]*len(self.tasks)
+        return alpha
+
+    def MGDA_approx_backward(self, losses, mgda_gn='none'):
+        if isinstance(self.rep, list):
+            batch_size = self.rep[0].size()[0]
+            rep_size = self.rep[0].size()
+        else:
+            batch_size = self.rep.size()[0]
+            rep_size = self.rep.size()
+        per_loss_grads = torch.zeros(len(self.tasks), *rep_size).cuda()
+        for tn, loss in enumerate(losses):
+            loss.backward(retain_graph=True)
+            per_loss_grads[tn] = self.rep_i[tn].grad.data.clone()
+
+        grads = {tn:per_loss_grads[tn] for tn in range(len(self.tasks))}
+        loss_data = {tn:losses[tn].item() for tn in range(len(self.tasks))}
+        gn = gradient_normalizers(grads, loss_data, normalization_type=mgda_gn)
+        for tn in range(len(self.tasks)):
+            grads[tn] = grads[tn] / gn[tn]
+        sol, _ = MinNormSolver.find_min_norm_element([grads[tn] for tn in range(len(self.tasks))])
+        if isinstance(self.rep, list):
+            for tn in range(len(self.tasks)):
+                self.rep[tn].backward(float(sol[tn])*per_loss_grads[tn], retain_graph=True)
+        else:
+            transformed_grad = sum(float(sol[tn])*per_loss_grads[tn] for tn in range(len(self.tasks)))
+            self.rep.backward(transformed_grad)
+        self.rep_i = [0]*len(self.tasks)
+        return torch.Tensor(sol).cuda()
+    
+    def GradNorm_backward(self, losses, L_0, alpha=1.5):
+        loss_scale = len(self.tasks)/self.loss_scale.sum()*self.loss_scale
+        if isinstance(self.rep, list):
+            batch_size = self.rep[0].size()[0]
+            rep_size = self.rep[0].size()
+        else:
+            batch_size = self.rep.size()[0]
+            rep_size = self.rep.size()
+        per_loss_grads = torch.zeros(len(self.tasks), *rep_size).cuda()
+        for tn, loss in enumerate(losses):
+            loss.backward(retain_graph=True)
+            per_loss_grads[tn] = self.rep_i[tn].grad.data.clone()
+        grads = per_loss_grads.reshape(len(self.tasks), batch_size, -1).sum(1).detach()
+        G_per_loss = torch.norm(loss_scale.unsqueeze(1)*grads, p=2, dim=-1)
+        G = G_per_loss.mean(0)
+        L_i = torch.Tensor([losses[tn].item()/L_0[tn] for tn in range(len(self.tasks))])
+        r_i = L_i/L_i.mean()
+        constant_term = (G*(r_i[tn]**alpha)).detach()
+        L_grad = sum([torch.abs(G_per_loss[tn]-constant_term) for tn in range(len(self.tasks))])
+        L_grad.backward()
+        loss_weight = loss_scale.detach().clone()
+        if isinstance(self.rep, list):
+            for tn in range(len(self.tasks)):
+                self.rep[tn].backward(loss_weight[tn]*per_loss_grads[tn], retain_graph=True)
+        else:
+            transformed_grad = sum(loss_weight[tn]*per_loss_grads[tn] for tn in range(len(self.tasks)))
+            self.rep.backward(transformed_grad)
+    
+    def get_share_params(self):
+        return self.backbone_s.parameters()
+        
+    def get_adaptative_parameter(self):
+        return self.alpha
