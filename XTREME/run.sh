0094102d926374d9c9b31c227d77278282e987a2
gpx <1457495424@qq.com>
2021-12-16 09:14:45 +0800

    add smtl

--- XTREME/run.sh
+++ XTREME/run.sh
@@ -9,7 +9,13 @@ python train.py --gpu_id 0 --model STL --dataset udpos --lang vi  > out/stl_pos_
 
 python train.py --gpu_id 1 --model DMTL --dataset udpos > out/dmtl_pos_all.out
 
-python train.py --gpu_id 2 --model SMTL --version v1 --dataset udpos > out/smtl_v1_pos_all.out
+python train.py --gpu_id 1 --model SMTL --version v1 --dataset udpos > out/smtl_v1_pos_all.out
+
+python train.py --gpu_id 1 --model SMTL --version v3 --dataset udpos > out/smtl_v3_pos_all.out
+
+python train.py --gpu_id 1 --model SMTL_new --version v1 --dataset udpos > out/smtl_new_v1_pos_all.out
+
+python train.py --gpu_id 1 --model SMTL_new --version v3 --dataset udpos > out/smtl_new_v3_pos_all.out
 
 # for PI task
 python train.py --gpu_id 3 --model STL --dataset pawsx --lang en  > out/stl_pawsx_en.out
@@ -22,6 +28,14 @@ python train.py --gpu_id 6 --model STL --dataset pawsx --lang es  > out/stl_paws
 
 python train.py --gpu_id 0 --model DMTL --dataset pawsx > out/dmtl_pawsx_all.out
 
+python train.py --gpu_id 1 --model SMTL --version v1 --dataset pawsx > out/smtl_v1_pawsx_all.out
+
+python train.py --gpu_id 1 --model SMTL --version v3 --dataset pawsx > out/smtl_v3_pawsx_all.out
+
+python train.py --gpu_id 1 --model SMTL_new --version v1 --dataset pawsx > out/smtl_new_v1_pawsx_all.out
+
+python train.py --gpu_id 1 --model SMTL_new --version v3 --dataset pawsx > out/smtl_new_v3_pawsx_all.out
+
 # for NER task
 python train.py --gpu_id 0 --model STL --dataset panx --lang en  > out/stl_panx_en.out
 
@@ -32,3 +46,11 @@ python train.py --gpu_id 2 --model STL --dataset panx --lang de  > out/stl_panx_
 python train.py --gpu_id 5 --model STL --dataset panx --lang es  > out/stl_panx_es.out
 
 python train.py --gpu_id 1 --model DMTL --dataset panx > out/dmtl_panx_all.out
+
+python train.py --gpu_id 1 --model SMTL --version v1 --dataset panx > out/smtl_v1_panx_all.out
+
+python train.py --gpu_id 1 --model SMTL --version v3 --dataset panx > out/smtl_v3_panx_all.out
+
+python train.py --gpu_id 1 --model SMTL_new --version v1 --dataset panx > out/smtl_new_v1_panx_all.out
+
+python train.py --gpu_id 1 --model SMTL_new --version v3 --dataset panx > out/smtl_new_v3_panx_all.out
